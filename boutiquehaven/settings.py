39f54ab25e13ce1a0a11a590175ff1ac23b12a77
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-12-27 12:10:08 +0000

    Added s3 bucket configs to settings.py

--- boutiquehaven/settings.py
+++ boutiquehaven/settings.py
@@ -211,15 +211,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket config
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_STORAGE_BUCKET = 'app-boutiquehaven'
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'app-boutiquehaven'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-  
-
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
